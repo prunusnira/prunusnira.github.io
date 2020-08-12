adced27642655d9df3c29bbf320438814083c785
Prunus Nira <prunusnira@gmail.com>
2020-08-12 16:12:18 +0900

    change to new homepage

---
+++ precache-manifest.038a66f92947bf8c40c61a68d225ef21.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "561ec3d331d404bd26b669145ce14e71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97caaa48d86bb45c84b2",
+    "url": "/static/css/2.3ed69fa8.chunk.css"
+  },
+  {
+    "revision": "9dac9d773b46e8e9ff43",
+    "url": "/static/css/main.c31a240c.chunk.css"
+  },
+  {
+    "revision": "97caaa48d86bb45c84b2",
+    "url": "/static/js/2.727f8dd0.chunk.js"
+  },
+  {
+    "revision": "836687f75dbf024cab882ad1c8c0e52b",
+    "url": "/static/js/2.727f8dd0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9dac9d773b46e8e9ff43",
+    "url": "/static/js/main.d53c360e.chunk.js"
+  },
+  {
+    "revision": "60b3c7cfc95554eadbb3",
+    "url": "/static/js/runtime-main.8ac15d18.js"
+  },
+  {
+    "revision": "050b6064618b41a575f07061dbc3b327",
+    "url": "/static/media/SquarePlus.050b6064.woff"
+  },
+  {
+    "revision": "127f6f9bb73141af1708415964dfb19d",
+    "url": "/static/media/SquarePlusR.127f6f9b.ttf"
+  },
+  {
+    "revision": "297447b23bd09bfdc5e1deefd35dd6e4",
+    "url": "/static/media/SquarePlusR.297447b2.woff2"
+  },
+  {
+    "revision": "c17c2cad17c758dd9f202d81ad53f042",
+    "url": "/static/media/back.c17c2cad.jpg"
+  }
+]);
